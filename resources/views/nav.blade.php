1879c8be0be207299b8510abe3790c7652f8aed1
Junaid mugloo <junaidmugloo@gmail.com>
2025-09-13 17:53:06 +0530

    ss

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -132,6 +132,13 @@
 <script src="https://elfsightcdn.com/platform.js" async></script>
 <div class="elfsight-app-31a13f8f-ab8f-47d6-9f95-21def2003318" data-elfsight-app-lazy></div>
 
+<style>
+    .scrollable-container__ScrollableContainer-sc-4a637efc-0 footer__ScrollableContent-sc-c5b178b6-2 ewVQBM ecdUAx
+    {
+
+        display: none !important;
+    }
+</style>
 
 <!-- Elfsight AI Chatbot | Untitled AI Chatbot -->
 <script src="https://elfsightcdn.com/platform.js" async></script>
