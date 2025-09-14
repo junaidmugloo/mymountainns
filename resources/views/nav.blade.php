005e891a1bb1129a9439540d983ed6e9f477cfe8
Junaid mugloo <junaidmugloo@gmail.com>
2025-09-14 12:12:30 +0530

    s

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -140,8 +140,8 @@
                                 </nav>
                             </div>
                         </div>
-                        <a href="{{ url("#0") }}" class="search-trigger search-icon"><i class="fa-regular fa-magnifying-glass"></i></a>
-                        <a href="javascript:void(0)" class="theme-btn"> Request A Quote <i class="fa-sharp fa-regular fa-arrow-right"></i></a>
+                        <a href="{{ url("#0") }}" class="search-trigger search-icon"><i class="fa-regular fa-user"></i></a>
+                        <a href="javascript:void(0)" class="theme-btn"> Login <i class="fa-sharp fa-regular fa-arrow-right"></i></a>
                         <div class="header__hamburger d-xl-none my-auto">
                             <div class="sidebar__toggle">
                                 <i class="fas fa-bars"></i>
