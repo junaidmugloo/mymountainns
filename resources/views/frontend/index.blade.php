e169a05dc79dfa0bb09bf41aa33ea5ef04e6aaac
Junaid mugloo <junaidmugloo@gmail.com>
2025-09-16 12:46:12 +0530

    xx

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -697,8 +697,7 @@
                             </div>
                             <a href="{{ url("tour-details.html") }}" class="theme-btn">BOOK NOW <i class="fa-sharp fa-regular fa-arrow-right"></i></a>
                             <div class="cta-image">
-                                <img src="{{ asset("offer_images/".$o->image) }}" alt="img">
-
+                                <img src="{{ asset("mymountains/assets/img/bag-shape.png") }}" alt="img">
                             </div>
                         </div>
                     </div>
