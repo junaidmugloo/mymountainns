631a03f934b864ca0d22bd91510c22007a9db243
Junaid mugloo <junaidmugloo@gmail.com>
2025-09-26 12:26:05 +0530

    d

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1982,7 +1982,7 @@ Sameer Arora</h4>
   </div>
 </div>
  {{-- <script src="https://ddosnotification.github.io/snow-theme/snow.js"></script> --}}
- <script>
+ {{-- <script>
  
 (function() {
     // Create and inject CSS
@@ -2100,7 +2100,9 @@ Sameer Arora</h4>
         createSnowflake: createSnowflake
     };
 })();
- </script>
+ </script> --}}
+
+ 
 <script>
   document.addEventListener('DOMContentLoaded', function() {
     // Initialize Bootstrap components
